284163c115c7611013d95491884bd879661ce6fb
Adrian-Mendez-Alcudia <98040217+Adrian-Mendez-Alcudia@users.noreply.github.com>
2024-05-09 22:20:54 -0600

    Actualizacion tarea

---
+++ js/slider.js
@@ -0,0 +1,45 @@
+(function(){
+
+    const sliders = [...document.querySelectorAll('.testimony__body')];
+    const buttonNext = document.querySelector('#next');
+    const buttonBefore = document.querySelector('#before');
+    let value;   
+    let intervalId;
+
+    buttonNext.addEventListener('click', ()=>{
+        changePosition(1);
+    });
+
+    buttonBefore.addEventListener('click', ()=>{
+        changePosition(-1);
+    });
+
+    const startAutoSlider = ()=>{
+        intervalId = setInterval(()=>{
+            changePosition(1);
+        }, 5500); //
+    }
+
+    const stopAutoSlider = ()=>{
+        clearInterval(intervalId);
+    }
+
+    startAutoSlider();
+
+    const changePosition = (add)=>{
+        const currentTestimony = document.querySelector('.testimony__body--show').dataset.id;
+        value = Number(currentTestimony);
+        value+= add;
+
+        sliders[Number(currentTestimony)-1].classList.remove('testimony__body--show');
+        if(value === sliders.length+1 || value === 0){
+            value = value === 0 ? sliders.length  : 1;
+        }
+
+        sliders[value-1].classList.add('testimony__body--show');
+
+        stopAutoSlider();
+        startAutoSlider();
+    }
+
+})();
